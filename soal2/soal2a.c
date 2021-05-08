61429e23f9cbd7494ce71130adf8584ddb0df087
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-05-08 21:07:44 +0700

    updated no 2

--- soal2/soal2a.c
+++ soal2/soal2a.c
@@ -8,32 +8,33 @@
 #include<sys/shm.h>
 #include<sys/ipc.h>
 
-pthread_t tid[5];
+pthread_t t_id[5];
 
-// Ubah-ubah matriks disini!!!
 int A[4][3] = { {1, 2, 3},
                 {3, 4, 5},
                 {5, 6, 7},
-                {7, 8, 9} },
+                {7, 8, 9} 
+              },
     B[3][6] = { {1, 3, 5, 7,  9, 11},
                 {2, 4, 6, 8, 10, 12},
-                {13, 14, 15, 16, 17, 18} },
+                {13, 14, 15, 16, 17, 18} 
+              },
     C[4][6];
 
-void* playandcount (void *arg) {
+void* startcount (void *arg) {
 
     pthread_t id = pthread_self();
 
-    if (pthread_equal(id, tid[0]))      // baris 1 matriks c
+    if (pthread_equal(id, t_id[0]))      // baris 1 matriks c
         for (int i = 0; i < 6; i++) 
             C[0][i] = A[0][0]*B[0][i] + A[0][1]*B[1][i] + A[0][2]*B[2][i];
-    else if (pthread_equal(id, tid[1])) // baris 2 matriks c
+    else if (pthread_equal(id, t_id[1])) // baris 2 matriks c
         for (int i = 0; i < 6; i++)
             C[1][i] = A[1][0]*B[0][i] + A[1][1]*B[1][i] + A[1][2]*B[2][i];
-    else if (pthread_equal(id, tid[2])) // baris 3 matriks c
+    else if (pthread_equal(id, t_id[2])) // baris 3 matriks c
         for (int i = 0; i < 6; i++)
             C[2][i] = A[2][0]*B[0][i] + A[2][1]*B[1][i] + A[2][2]*B[2][i];
-    else if (pthread_equal(id, tid[3])) // baris 4 matriks c
+    else if (pthread_equal(id, t_id[3])) // baris 4 matriks c
         for (int i = 0; i < 6; i++)
             C[3][i] = A[3][0]*B[0][i] + A[3][1]*B[1][i] + A[3][2]*B[2][i];
 
@@ -41,38 +42,35 @@ void* playandcount (void *arg) {
 
 int main () {
 
-    for (int i = 0; i <5; i++) {
-        int err = pthread_create(&(tid[i]), NULL, &playandcount, NULL);
-
-        // if (err != 0)
-        //     printf("\nCan't create thread: [%s]", strerror(err));
-    
+    int i, j, err;
+    for (i = 0; i < 4; i++) {
+        err = pthread_create(&(t_id[i]), NULL, &startcount, NULL);
     }
 
-    pthread_join(tid[0], NULL);
-    pthread_join(tid[1], NULL);
-    pthread_join(tid[2], NULL);
-    pthread_join(tid[3], NULL);
+    pthread_join(t_id[0], NULL);
+    pthread_join(t_id[1], NULL);
+    pthread_join(t_id[2], NULL);
+    pthread_join(t_id[3], NULL);
 
-    printf("MATRIKS PERKALIAN\n");
+    printf("MATRIX RESULT\n");
 
     int row = 4;
-    int column = 6;
+    int col = 6;
     int *matrix;
 
     key_t key = 1234;
-    int shmid = shmget(key, sizeof(int)*row*column, IPC_CREAT | 0666);
+    int shmid = shmget(key, sizeof(int)*row*col, IPC_CREAT | 0666);
     matrix = (int *)shmat(shmid, NULL, 0);
 
-    for (int i = 0; i < row; i++){
-        for (int j = 0; j < column; j++){
-            matrix[i*column + j] = C[i][j];
-            printf("%d\t", matrix[i*column + j]);
+    for (i = 0; i < row; i++){
+        for (j = 0; j < col; j++){
+            matrix[i*col + j] = C[i][j];
+            printf("%d\t", matrix[i*col + j]);
         }
         printf("\n");   
     }
 
-    sleep(10);
+    sleep(5);
 
     shmdt(matrix);
     shmctl(shmid, IPC_RMID, NULL);
