85717ccd4776d91bbabf8a19c33a1c38931ce018
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-05-22 15:17:30 +0700

    Revert "revised 2c and 3"
    This reverts commit 1ef6e571f8af3f21358b76651f1f68d65d23fb20.

--- soal2/soal2a.c
+++ soal2/soal2a.c
@@ -8,7 +8,7 @@
 #include<sys/shm.h>
 #include<sys/ipc.h>
 
-pthread_t t_id[24];
+pthread_t t_id[5];
 
 int A[4][3] = { {1, 1, 1},
                 {1, 2, 3},
@@ -25,37 +25,7 @@ void* startcount (void *arg) {
 
     pthread_t id = pthread_self();
 
-for (int i = 0; i < 24 ; i++){
-  
-  for (int j = 0; j < 6; j++){
-  	if (pthread_equal(id, t_id[i])){      // baris 1 matriks c
-  	C[0][j] = A[0][0]*B[0][j] + A[0][1]*B[1][j] + A[0][2]*B[2][j];
-  	}
-  	i++;
-   }
-	
-   for (int j = 0; j < 6; j++){
-    	if (pthread_equal(id, t_id[i])){      // baris 2 matriks c
-    	C[1][j] = A[1][0]*B[0][j] + A[1][1]*B[1][j] + A[1][2]*B[2][j];
-    	}
-    	i++;
-   }
-   
-    for (int j = 0; j < 6; j++){
-    	if (pthread_equal(id, t_id[i])){      // baris 3 matriks c
-    	C[2][j] = A[2][0]*B[0][j] + A[2][1]*B[1][j] + A[2][2]*B[2][j];
-    	}
-    	i++;
-   }
-   
-    for (int j = 0; j < 6; j++){
-    	if (pthread_equal(id, t_id[i])){      // baris 4 matriks c
-    		C[3][j] = A[3][0]*B[0][j] + A[3][1]*B[1][j] + A[3][2]*B[2][j];
-    	}
-    	i++;
-   }
-   
-    /*if (pthread_equal(id, t_id[0]))      // baris 1 matriks c
+    if (pthread_equal(id, t_id[0]))      // baris 1 matriks c
         for (int i = 0; i < 6; i++) 
             C[0][i] = A[0][0]*B[0][i] + A[0][1]*B[1][i] + A[0][2]*B[2][i];
     else if (pthread_equal(id, t_id[1])) // baris 2 matriks c
@@ -67,27 +37,21 @@ for (int i = 0; i < 24 ; i++){
     else if (pthread_equal(id, t_id[3])) // baris 4 matriks c
         for (int i = 0; i < 6; i++)
             C[3][i] = A[3][0]*B[0][i] + A[3][1]*B[1][i] + A[3][2]*B[2][i];
-	*/
-   }
+
 }
 
 int main () {
 
     int i, j, err;
-    for (i = 0; i < 24; i++) {
+    for (i = 0; i < 4; i++) {
         err = pthread_create(&(t_id[i]), NULL, &startcount, NULL);
     }
 
-    for (i = 0; i < 24; i++) {
-	    pthread_join(t_id[i], NULL);
-    }
-
-/*
     pthread_join(t_id[0], NULL);
     pthread_join(t_id[1], NULL);
     pthread_join(t_id[2], NULL);
     pthread_join(t_id[3], NULL);
-*/
+
     printf("MATRIX RESULT\n");
 
     int row = 4;
